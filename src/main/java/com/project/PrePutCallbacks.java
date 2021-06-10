a14d2e01e62e3e115788969cde8b908eaf97bca8
MALKARAJ-FULLCREATIVE <malkaraj.karup@anywhere.co>
2021-06-10 11:42:34 +0000

    from gcp

--- src/main/java/com/project/PrePutCallbacks.java
+++ src/main/java/com/project/PrePutCallbacks.java
@@ -3,8 +3,6 @@ import com.google.appengine.api.datastore.PrePut;
 import com.google.appengine.api.datastore.PutContext;
 import com.google.appengine.repackaged.org.joda.time.DateTime;
 
-import jdk.internal.org.jline.utils.Log;
-
 import java.util.logging.Logger;
 import java.util.Date;
 public class PrePutCallbacks {
@@ -17,7 +15,7 @@ private static final Logger log = Logger.getLogger(PrePutCallbacks.class.getName
     @PrePut
     void updateTimestamp(PutContext context) {
       
-        Log.info("updateTimeStamp");
+        log.info("updateTimeStamp");
 		DateTime d = new DateTime();
 	        context.getCurrentElement().setProperty("updated", d.getMillis());
     }
