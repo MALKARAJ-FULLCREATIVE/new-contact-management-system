ad9ff2ff7155e9982cf9b54b5af3a4b966c9b304
MALKARAJ-FULLCREATIVE <malkaraj.karup@anywhere.co>
2021-06-25 07:45:59 +0000

    exit

--- src/main/java/com/project/RegistrationPage.java
+++ src/main/java/com/project/RegistrationPage.java
@@ -7,6 +7,7 @@ import java.io.PrintWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 import java.util.logging.Logger;
 
 import javax.servlet.ServletException;
@@ -15,6 +16,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.appengine.api.datastore.Email;
 import com.google.appengine.api.urlfetch.HTTPHeader;
 import com.google.appengine.api.urlfetch.HTTPMethod;
 import com.google.appengine.api.urlfetch.HTTPRequest;
@@ -30,86 +32,83 @@ import org.mindrot.jbcrypt.BCrypt;
  */
 @WebServlet("/register")
 public class RegistrationPage extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-	private static final Logger log = Logger.getLogger("logger");
-	/**
-	 * @see HttpServlet#HttpServlet()
-	 */
-	public RegistrationPage() {
-		super();
-		// TODO Auto-generated constructor stub
+    private static final long serialVersionUID = 1L;
+    private static final Logger log = Logger.getLogger("logger");
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public RegistrationPage() {
+        super();
+        // TODO Auto-generated constructor stub
     }
-    
 
     @Override
-    protected void doOptions(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException 
-    { 
+    protected void doOptions(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         // pre-flight request processing
         resp.setHeader("Access-Control-Allow-Origin", "*");
         resp.setHeader("Access-Control-Allow-Methods", "*");
         resp.setHeader("Access-Control-Allow-Headers", "*");
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
-	 *      response)
-	 */
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+     *      response)
+     */
 
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        // TODO Auto-generated method stub
 
+        request.getRequestDispatcher("register.jsp").forward(request, response);
 
+    }
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-		// TODO Auto-generated method stub
+    /**
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+     *      response)
+     */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        // TODO Auto-generated method stub
 
-		request.getRequestDispatcher("register.jsp").forward(request, response);
+        response.setContentType("application/json");
+        // response.setHeader("Access-Control-Allow-Origin","*");
+        response.addHeader("Access-Control-Allow-Origin", "*"); // george
 
-	}
+        StringBuffer jb = new StringBuffer();
+        String line = null;
+        try {
+            BufferedReader reader = request.getReader();
+            while ((line = reader.readLine()) != null)
+                jb.append(line);
+        } catch (Exception e) {
+            /* report an error */ }
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
-	 *      response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-		// TODO Auto-generated method stub 
+        String str = jb.toString();
+        JSONObject jsonobject = new JSONObject(str);
 
-		response.setContentType("application/json");
-        //response.setHeader("Access-Control-Allow-Origin","*");
-        response.addHeader("Access-Control-Allow-Origin","*"); //george
+        String origin = request.getHeader("Origin");
 
+        String email = jsonobject.getString("email");
+        String pwd = BCrypt.hashpw(jsonobject.get("password").toString(), BCrypt.gensalt());
 
+        User user = new User(email, pwd);
+
+        UUID id = UUID.randomUUID();
+
+        if (origin != null && (origin.equals("https://malkarajtraining12.uc.r.appspot.com")
+                || origin.equals("http://localhost:8080"))) {
+            user.setUser_id(id.toString());
+        } else {
+            try {
+                TimeUnit.SECONDS.sleep(5);
+            } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        String user_id=jsonobject.get("user_id").toString();
         
-          
-
-		StringBuffer jb = new StringBuffer();
-		String line = null;
-		try {
-			BufferedReader reader = request.getReader();
-			while ((line = reader.readLine()) != null)
-				jb.append(line);
-		} catch (Exception e) {
-			/* report an error */ }
-
-		String str = jb.toString();
-		JSONObject jsonobject = new JSONObject(str);
-		
-		String origin = request.getHeader("Origin");
-
-		String email = jsonobject.getString("email");
-		String pwd = BCrypt.hashpw(jsonobject.get("password").toString(), BCrypt.gensalt());
-
-		User user = new User(email, pwd);
-		
-		UUID id= UUID.randomUUID();
-		
-		if(origin!=null && (origin.equals("https://malkarajtraining12.uc.r.appspot.com") || origin.equals("http://localhost:8080")))
-		{
-		user.setUser_id(id.toString());
-		}
-		else
-		{
-		String user_id=jsonobject.get("user_id").toString();
 		user.setUser_id(user_id.toString());
 
 		            }
@@ -132,14 +131,8 @@ public class RegistrationPage extends HttpServlet {
 											final String uri = "https://georgefulltraining12.uc.r.appspot.com/register";
 											URL url = new URL(uri);
 											// HTTPRequest req = new HTTPRequest(url, HTTPMethod.POST);
-											HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-
-											conn.setDoOutput(true);
-											conn.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
-											conn.setRequestProperty("Accept", "application/json");
 											// Set HTTP request method.
-											conn.setRequestMethod("POST");
-											conn.setConnectTimeout(2);
+											
 											
 								//	req.addHeader(new HTTPHeader("Authorization", BCrypt.hashpw(SyncApp.sentKey,BCrypt.gensalt(10))));
                                 // req.addHeader(new HTTPHeader("Origin","https://georgefulltraining12.uc.r.appspot.com"));
@@ -150,59 +143,40 @@ public class RegistrationPage extends HttpServlet {
 											  reqObj.put("password", pwd);
 											  reqObj.put("user_id", user.getUser_id());
 											  
-											  OutputStreamWriter writer = new OutputStreamWriter(conn.getOutputStream());
-											  writer.write(reqObj.toString());
-											  writer.close();
+											//  OutputStreamWriter writer = new OutputStreamWriter(conn.getOutputStream());
+											 // writer.write(reqObj.toString());
+											  //writer.close();
 											  
 											//  req.setPayload(reqObj.toString().getBytes());
-											  //obj1=SyncApp.sentRequest(req);
-											  if(obj1.get("success").toString().equals("true"))
-												{
-												log.info("User succesfully registered in cross domain");
-													obj1.put("detail", email);
-													response.setStatus(200);
-												}
-											  else
-												{
-													log.severe("User registration failed due to exceeding retry limit");
-													response.setStatus(500);
-												}						
-										}
-										else
-										{
-											obj1.put("success", true);
-											obj1.put("code", 200);
-											obj1.put("detail", email);
-										}
-            
-         
-			out.println(obj1);
-			
+                                              //obj1=SyncApp.sentRequest(req);
+                                             
+                                              //int respCode = conn.getResponseCode(); // New items get NOT_FOUND on PUT
 
-				
-//       final String u="https://georgefulltraining12.uc.r.appspot.com/register";
-//         
-//                    URL url=new URL(u);
-//                     
-//					HTTPRequest req = new HTTPRequest(url, HTTPMethod.POST);
-//					JSONObject reqObj=new JSONObject();
-//					reqObj.put("email", email);
-//					reqObj.put("password", pwd);
-//                
-//                    req.setPayload(reqObj.toString().getBytes());
-//					HTTPResponse res = SyncApp.fetcher.fetch(req);
-//					
-//         
-//                    
-//			JSONObject obj = new JSONObject();
-//			JSONObject obj1 = new JSONObject();
-//			response.setStatus(200);
-//			obj1.put("email", email);
-//			obj.put("success", true);
-//			obj.put("code", 200);
-//			obj.put("message", "registration successfull!");
-//			obj.put("user", obj1);
-//			out.println(obj);
+                                        obj1=SyncApp.sentRequest(url, reqObj);
+                                        if(obj1.get("success").toString().equals("true"))
+                                        {
+                                        log.info("User succesfully registered in cross domain");
+                                        obj1.put("detail", email);
+                                        response.setStatus(200);
+                                        }
+                                        else
+                                        {
+                                        log.severe("User registration failed due to exceeding retry limit");
+                                        response.setStatus(500);
+                                        }
+                                    }
+
+                                                                                            else
+                                            {
+                                            obj1.put("success", true);
+                                            obj1.put("code", 200);
+                                            obj1.put("detail", email);
+                                            }                       
+                                                        
+         
+            out.println(obj1);
+                
+		
 
 		} else {
 			JSONObject obj = new JSONObject();
@@ -222,4 +196,5 @@ public class RegistrationPage extends HttpServlet {
 
 	}
 
-}
+            }
+
--- src/main/java/com/project/SyncApp.java
+++ src/main/java/com/project/SyncApp.java
@@ -3,13 +3,19 @@ package com.project;
 import com.google.appengine.api.urlfetch.URLFetchService;
 import com.google.appengine.api.urlfetch.URLFetchServiceFactory;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.logging.Logger;
 
 import com.google.appengine.api.urlfetch.HTTPRequest;
 import com.google.appengine.api.urlfetch.HTTPResponse;
 
 import org.json.JSONObject;
+import org.mindrot.jbcrypt.BCrypt;
 
 public class SyncApp {
 	
@@ -21,21 +27,39 @@ public class SyncApp {
 	 static final String receiveKey = "9ac80b0c-d004-11eb-b8bc-0242ac130003";
      static final String sentKey = "0115daa6-d00a-11eb-b8bc-0242ac130003";
     
-    public static JSONObject sentRequest(HTTPRequest req) throws IOException
+     public static JSONObject sentRequest(URL url, JSONObject obj) throws IOException
 	 {
+        //SyncApp sync=new SyncApp();
+    	HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+    	conn.setDoOutput(true);
+    	conn.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
+        conn.setRequestProperty("Accept", "application/json");
+        conn.setRequestProperty("Authorization",  BCrypt.hashpw(sentKey,BCrypt.gensalt(10)));
+    	conn.setRequestMethod("POST");
+    	conn.setReadTimeout(30000);
+    	StringBuilder response =null;
 		JSONObject obj1=new JSONObject();
 		int code = 500;
 		int retryLimit=3;
-		HTTPResponse res = null;
-        int i=0;
-
-
-        while(i<retryLimit)
+		int i=0;
+		while(i<retryLimit)
 		{
-			log.info("registration attempt no : "+ i);
-			res = fetcher.fetch(req);
-			
-            code= res.getResponseCode();
+			log.info("registration attempt : "+ i);
+			OutputStreamWriter writer = new OutputStreamWriter(conn.getOutputStream());
+			writer.write(obj.toString());
+			writer.close();
+            code = conn.getResponseCode(); 
+            
+            response = new StringBuilder();
+            String line;
+            BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            
+            while ((line = reader.readLine()) != null) {
+            response.append(line);
+            }
+
+            reader.close();
+            
             log.info("code :"+code);
 			if(code>=200 && code<300)
 			{
@@ -49,7 +73,9 @@ public class SyncApp {
 				i++;
 			}
 		}
-		JSONObject json=new JSONObject(res);
+
+        JSONObject json=new JSONObject(response.toString());
+        log.info("code :"+json.get("code").toString());
 		if(i>2)
 		{
 			obj1.put("message", json.get("message"));
@@ -58,7 +84,6 @@ public class SyncApp {
 		}
 		return obj1;
 	 }
-
 
 
 }
